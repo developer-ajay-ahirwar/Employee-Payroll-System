61d5f49241fba584eeb533e92ac995702c0e3b85
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-14 09:34:03 +0530

    Write a PayRollSyatem version 10.4

--- PayRollSyatem.java
+++ PayRollSyatem.java
@@ -1,3 +1,35 @@
-public class PayRollSyatem {
 
-}   
+import java.util.ArrayList;
+
+public class PayRollSyatem {
+    private ArrayList<Emlpoyee> emlpoyeeList;
+
+    public PayRollSyatem(){
+        emlpoyeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Emlpoyee emlpoyee){
+        emlpoyeeList.add(emlpoyee);
+    }
+    public void removeEmployee(int id){
+        Emlpoyee emlpoyeeToremove = null;
+       for (Emlpoyee emlpoyee : emlpoyeeList) {
+           if(emlpoyee.getId() == id){
+                emlpoyeeToremove = emlpoyee;
+                break;
+           }
+           else{
+                System.out.println("Invaild id Number");
+           }
+       }
+       if(emlpoyeeToremove != null){
+            emlpoyeeList.remove(emlpoyeeToremove);
+       }
+       
+    }
+    public void desplayEmployee(){
+        for(Emlpoyee emlpoyee : emlpoyeeList){
+            System.out.println(emlpoyee);
+        }
+    }
+}  
