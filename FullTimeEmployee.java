7873436bd3c3e980b0529d1d42aa172ec1d747f7
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-14 09:00:07 +0530

    Write a FullTimeEmlpoyee version 1.02

--- FullTimeEmployee.java
+++ FullTimeEmployee.java
@@ -1,3 +1,12 @@
-public class FullTimeEmployee {
+public class FullTimeEmployee extends Emlpoyee {
+    private double mothlySalary;
 
+    public FullTimeEmployee(String name , int id , double mothlySalary){
+        super(name,id);
+        this.mothlySalary = mothlySalary;
+    }
+    @Override
+    public double calculateSalary(){
+        return mothlySalary;
+    }
 }
