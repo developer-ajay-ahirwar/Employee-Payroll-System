9d923d98974bcf07104bd8e601bf0a039a260f63
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-14 08:46:22 +0530

    Write a employee class version 1.01

--- Emlpoyee.java
+++ Emlpoyee.java
@@ -1,4 +1,21 @@
 public abstract class Emlpoyee {
-   
+    private String name;
+    private int id;
+    public Emlpoyee(String name , int id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public abstract double calculateSalary();
+    @Override
+    public String toString(){
+        return "Employee{name="+name+" \n id="+id+" \n Salary="+calculateSalary()+"}";
+    }
 
 }
