6080accba8b26a38d81b915ff971df64716d1329
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-14 09:01:31 +0530

    Write a PartTmeEmployee verison 1.03

--- PartTimeEmployee.java
+++ PartTimeEmployee.java
@@ -1,2 +1,14 @@
 public class PartTimeEmployee extends Emlpoyee{
+    private int hoursWorld;
+    private double hourlyRate;
+    public PartTimeEmployee(String name , int id , int hoursWorld , double hourlyRate){
+        super(name, id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorld = hoursWorld;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return hourlyRate * hoursWorld;
+    }
 }
