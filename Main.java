2b72609ede4f8aa34b173c452574032e33e516c8
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-06-14 11:58:27 +0530

    Write a Main class version 2.0

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
-
+    public static void main(String[] args){
+        PayRollSyatem payRollSyatem = new PayRollSyatem();
+        FullTimeEmployee fEmployee_1 = new FullTimeEmployee("Ajay", 001, 100000);
+        payRollSyatem.addEmployee(fEmployee_1);
+        PartTimeEmployee pEmployee_1 = new PartTimeEmployee("Rohit", 101, 51, 25);
+        payRollSyatem.addEmployee(pEmployee_1);
+        payRollSyatem.desplayEmployee();
+    }
 }
